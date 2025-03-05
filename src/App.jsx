fedceeec389eb9559661cca718417de6ffa21a97
risch24 <riskafadlun2@gmail.com>
2025-03-05 14:52:42 +0800

    Inisialisasi Git dan tambahkan semua file proyek

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProdukList from './components/ProdukList';
+
+function App() {
+  return (
+    <div>
+      <h1>Selamat Datang di Aplikasi E-Commerce Sederhana</h1>
+      <ProdukList />
+    </div>
+  );
+}
+
+export default App;
