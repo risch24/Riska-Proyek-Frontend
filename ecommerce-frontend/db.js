f1fe74c60d3a58132e2a81e1a0e104506beac3d6
intniaaa20 <10231029@student.itk.ac.id>
2025-03-15 21:59:34 +0800

    Add basic Express.js setup and routes

--- ecommerce-frontend/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({
- user: 'postgres',
- host: 'localhost',
- database: 'ecommerce-kecil',
- password: 'JANGANLUPALAGI20',
- port: 5432,
-});
-module.exports = pool;
