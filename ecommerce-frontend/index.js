d1d75204af30c82d5ab7dad59b56b8d113877047
intniaaa20 <10231029@student.itk.ac.id>
2025-03-15 21:06:31 +0800

    Add basic Express.js setup and routes

--- ecommerce-frontend/index.js
+++ ecommerce-frontend/index.js
@@ -1,16 +1,42 @@
+// gabungan kode dari modul 2 dan 3
 const express = require('express');
-const cors = require('cors');
+ const cors = require('cors');
+ const pool = require('./db');
  const app = express();
  const PORT = 3001;
  app.use(cors());
  app.use(express.json());
  app.get('/', (req, res) => {
- res.send('Hello World from Express.js!');
+  res.send('Hello World from Express.js!');
  });
  app.post('/data', (req, res) => {
- const { nama } = req.body;
- res.send(`Data diterima: ${nama}`);
+  const { nama } = req.body;
+  res.send(`Data diterima: ${nama}`);
  });
- app.listen(PORT, () => {
- console.log(`Server berjalan di http://localhost:${PORT}`);
+ // CREATE
+ app.post('/produk', async (req, res) => {
+  const { nama, harga } = req.body;
+  try {
+    const newProduk = await pool.query(
+      'INSERT INTO produk (nama, harga) VALUES ($1, $2) RETURNING *',
+      [nama, harga]
+    );
+    res.json(newProduk.rows[0]);
+} catch (err) {
+ console.error(err);
+ res.status(500).json({ error: 'Server error' });
+ }
  });
+// READ
+app.get('/produk', async (req, res) => {
+    try {
+    const allProduk = await pool.query('SELECT * FROM produk');
+    res.json(allProduk.rows);
+    } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+    }
+    });
+    app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+    });
